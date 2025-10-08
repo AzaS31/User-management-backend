37909fdb14fd497f6ea18ce2b2e72eff7a8b6513
Azamat <azamatsalaly@gmail.com>
2025-10-08 06:17:37 +0500

    test

--- controllers/authController.js
+++ controllers/authController.js
@@ -72,17 +72,3 @@ exports.login = async (req, res) => {
         res.status(500).json({ message: 'Login failed', error: err.message });
     }
 };
-
-exports.logout = (req, res) => {
-    try {
-        res.clearCookie('token', {
-            httpOnly: true,
-            secure: true,  
-            sameSite: 'None'
-        });
-        res.json({ message: 'Logged out successfully' });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Logout failed', error: err.message });
-    }
-};
